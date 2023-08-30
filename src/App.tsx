62fb088a4b2356f6cfae31a0353ddc1cce214885
Chineze Nwosu <nwosuchineze@gmail.com>
2023-08-30 18:45:38 +0100

    create app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import Room from './Room';
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    fetch("http://localhost:8000/message")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
-
   return (
-    <div className="App">
-      <h1>{message}</h1>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+              <Link to="/">Home</Link>
+          </li>
+        </ul>
+        <Routes>
+          <Route path='/' element={< Home />}></Route>
+          <Route path='/diary/:id' element={< Room />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
