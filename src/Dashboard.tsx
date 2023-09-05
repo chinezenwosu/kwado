7e5b94529dd94c9c8a172ac4570d54199de791a4
Chineze Nwosu <nwosuchineze@gmail.com>
2023-09-05 22:52:00 +0100

    move create document function to dashboard

---
+++ src/Dashboard.tsx
@@ -0,0 +1,62 @@
+import { Node } from 'slate'
+import { useNavigate } from 'react-router-dom'
+import { Button } from "./Components"
+import { v4 as uuid } from 'uuid'
+import axios from 'axios'
+import routes from './utils/routes'
+import { config } from './utils/config'
+import { useAuth } from './hooks/useAuth'
+
+const Dashboard = () => {
+  const { logout } = useAuth()
+  const navigate = useNavigate()
+
+  const onClick = async () => {
+    const defaultValue: Node[] = [
+      {
+        type: 'paragraph',
+        children: [
+          {
+            text: 'Dear Diary,'
+          }
+        ]
+      }
+    ]
+
+    try {
+      const documentRes = await axios.post(`${config.url.api}/kwadocs`, {
+        content: defaultValue,
+        slug: `${Date.now()}-${uuid()}`,
+      })
+
+      navigate(routes.getDiary(documentRes.data.slug))
+    }
+    catch(e) {
+      console.log('Document load error', e)
+    }
+  }
+
+  const handleLogout = async () => {
+    try {
+      await axios.post(`${config.url.api}/users/logout`, {}, { withCredentials: true })
+      logout()
+      window.location.reload()
+    }
+    catch(e) {
+      console.log('Logout error', e)
+    }
+  }
+
+  return (
+    <div>
+      <Button type="button" onClick={onClick}>
+        Write something
+      </Button>
+      <Button type="button" onClick={handleLogout}>
+        Logout
+      </Button>
+    </div>
+  )
+}
+
+export default Dashboard
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,53 +1,9 @@
-import { Node } from 'slate'
-import { useNavigate } from 'react-router-dom'
-import { Button } from "./Components"
-import { v4 as uuid } from 'uuid'
-import routes from './utils/routes'
-
 const Home = () => {
-  const navigate = useNavigate()
-
-  const onClick = async () => {
-    const defaultValue: Node[] = [
-      {
-        type: 'paragraph',
-        children: [
-          {
-            text: 'Dear Diary,'
-          }
-        ]
-      }
-    ]
-
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        content: defaultValue,
-        slug: `${Date.now()}-${uuid()}`
-      }),
-    }
-
-    try {
-      const documentRes = await fetch('http://localhost:8000/api/kwadocs', options)
-      const document = await documentRes.json()
-
-      navigate(routes.getDiary(document.slug))
-    }
-    catch(e) {
-      console.log('Document load error', e)
-    }
-  }
-
   return (
-    <div>
-      <Button type="button" onClick={onClick}>
-        Write something
-      </Button>
-    </div>
+    <h4>
+      Home
+    </h4>
   )
 }
 
-export default Home;
+export default Home
