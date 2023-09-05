050101bc663e65f8bcbe636cb46c0330358b3b3a
Chineze Nwosu <nwosuchineze@gmail.com>
2023-09-05 23:09:13 +0100

    add more route getters

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -2,6 +2,15 @@ const routes = {
   getHome: () => {
     return '/'
   },
+  getLogin: () => {
+    return '/login'
+  },
+  getSignup: () => {
+    return '/signup'
+  },
+  getDashboard: () => {
+    return '/dashboard'
+  },
   getDiary: (slug: string) => {
     return `/diary/${slug}`
   }
