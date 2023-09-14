7e3994a78fef556d75d79e2769800221877a65b8
Chineze Nwosu <nwosuchineze@gmail.com>
2023-09-14 01:03:25 +0100

    add websocket url config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,7 @@
 const config = {
   url: {
-    api: process.env.REACT_APP_API_URL,
+    api: process.env.REACT_APP_API_URL as string,
+    websocket: process.env.REACT_APP_WEBSOCKET_URL as string,
   }
 }
 
