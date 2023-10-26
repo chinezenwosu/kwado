56b29f92bd6a34d7cf05f524291323f453a8e67b
Chineze Nwosu <nwosuchineze@gmail.com>
2023-10-26 23:26:30 +0100

    remove unused components in dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,40 +1,13 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { getDiaries } from '../../api/editor'
-import DiaryList from './components/DiaryList'
-import CreateDiary from './components/CreateDiary'
+import React from 'react'
 import styles from './Dashboard.module.css'
 
-const Diary = () => {
-  const [diaries, setDiaries] = useState([])
-
-  useEffect(() => {
-    const fetchData = async () => {
-      return await handleFetchDiaries()
-    }
-
-    fetchData()
-  }, [])
-
-  const handleFetchDiaries = async () => {
-    const response = await getDiaries()
-
-    if (response.data) {
-      setDiaries(response.data)
-    }
-  }
+const Dashboard = () => {
 
   return (
     <div className={styles.dashboard}>
-      <div className={`${styles.container} ${styles.diaryListContainer}`}>
-        <CreateDiary />
-        <DiaryList diaries={diaries} />
-      </div>
-      <div className={styles.container}>
-        Hey there
-      </div>
+      <div className={styles.container} />
     </div>
   )
 }
 
-export default Diary
+export default Dashboard
